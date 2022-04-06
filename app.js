9fba7af4317948ee0eb980b473e3928f7887529e
Derma6 <dermaweb.dev@gmail.com>
2022-04-06 16:39:50 +0200

    modif

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const sequelize = require('./src/db/sequelize.js');
+const cors = require('cors')
 
 
 const app = express()
