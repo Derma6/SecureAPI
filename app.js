05d5529f955c09f79b07daeee7be4691aaf73acc
Derma6 <dermaweb.dev@gmail.com>
2022-04-06 17:28:53 +0200

    test

--- app.js
+++ app.js
@@ -13,23 +13,23 @@ app
     .use(bodyParser.json())
     .use(cors())
 
-sequelize.initDb()
+// sequelize.initDb()
 
 app.get('/', (req, res) => {
     res.json('Hello, Heroku ! 👋🏻 ')
 })
 
-require('./src/routes/login')(app)
+// require('./src/routes/login')(app)
 
-require('./src/routes/findAllClasses')(app)
-require('./src/routes/findClasseByPk')(app)
-require('./src/routes/createClasse')(app)
-require('./src/routes/updateClasse')(app)
-require('./src/routes/deleteClasse')(app)
+// require('./src/routes/findAllClasses')(app)
+// require('./src/routes/findClasseByPk')(app)
+// require('./src/routes/createClasse')(app)
+// require('./src/routes/updateClasse')(app)
+// require('./src/routes/deleteClasse')(app)
 
-app.use(({res}) => {
-    const message = 'Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.'
-    res.status(404).json({message})
-})
+// app.use(({res}) => {
+//     const message = 'Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.'
+//     res.status(404).json({message})
+// })
 
 app.listen(port) 
