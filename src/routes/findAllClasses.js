93c115c063c10aa1a3b68a1959779dab989323dd
Derma6 <dermaweb.dev@gmail.com>
2022-04-20 09:46:21 +0200

    test heroku

--- src/routes/findAllClasses.js
+++ src/routes/findAllClasses.js
@@ -4,13 +4,13 @@ const { Op } = require('sequelize')
 const auth = require('../auth/auth')
 
 module.exports = (app) => {
-    app.get('/api/dofus/classes', auth, (req, res) => {
-        if(req.query.name) {
+    app.get('/api/dofus/allClasses', auth, (req, res) => {
+        if (req.query.name) {
             const name = req.query.name
             const limit = parseInt(req.query.limit) || 5
-            
-            if(name.length<2){
-                return res.status(400).json({message: 'La recherche doit comporter au minimum 2 caractères.'})
+
+            if (name.length < 2) {
+                return res.status(400).json({ message: 'La recherche doit comporter au minimum 2 caractères.' })
             }
 
             return Personnage.findAndCountAll({
@@ -22,19 +22,19 @@ module.exports = (app) => {
                 order: ['name'],
                 limit: limit
             })
-            .then(({count, rows}) => {
-                const message = `Il y a ${count} classe correspondant au nom ${name}`
-                res.json({message, data: rows})
-            })
+                .then(({ count, rows }) => {
+                    const message = `Il y a ${count} classe correspondant au nom ${name}`
+                    res.json({ message, data: rows })
+                })
         }
-        Personnage.findAll({order: ['name'] })
+        Personnage.findAll({ order: ['name'] })
             .then(personnages => {
                 const message = 'La liste a bien été récupérée.'
-                res.json({message, data: personnages})
+                res.json({ message, data: personnages })
             })
             .catch(error => {
                 const message = `La liste des classes n'a pas pu être récupérée. Réessayez dans quelques instants.`
-                res.status(500).json({message, data: error})
+                res.status(500).json({ message, data: error })
             })
     })
 }
